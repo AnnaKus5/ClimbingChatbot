a184c7662514a656405c444de738a04dd692ae2b
Ania Kuś <aniaa.kus@gmail.comgit config user.namegit config --global user.email aniaa.kus@gmail.com>
2025-01-20 09:52:02 +0100

    Configure TS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { pool, testConnection } from './config/database.js';
-import routesRouter from './routes/routes.js';
+import routesRouter from './api/routes.js';
+
+console.log("start")
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use('/api/routes', routesRouter);
+// app.use('/api/routes', routesRouter);
 
-// Start serwera
 const startServer = async () => {
     try {
         await testConnection();
--- server/src/services/routeServices.ts
+++ server/src/services/routeServices.ts
@@ -1,11 +1,19 @@
 import pool from '../config/database.js';
 
+type route = {
+    name: string,
+    crag: string,
+    grade: number,
+    rating: number
+}
+
+
 export async function getRoutes() {
     const [routes] = await pool.query("SELECT * FROM routes LIMIT 10");
     return routes;
 }
 
-export async function addRoute(route) {
+export async function addRoute(route: route) {
     const {name, crag, grade, rating} = route;
     const [result] = await pool.query(`
         INSERT INTO routes (name, crag, grade, rating)
